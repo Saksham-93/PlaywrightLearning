442d38ce50c74612bae69c31df68795e72d0e704
Saksham <saksham.sahni@kickdrumtech.com>
2026-08-03 21:25:19 +0530

    Print Statement start of Typescript journey

---
+++ typescriptFundamentals/firstDemo.ts
@@ -0,0 +1,13 @@
+/**
+ * To download TypeScript, use the following npm command:
+ * npm install -g typescript
+ *
+ * TypeScript covers the major functionality of JavaScript and adds
+ * additional features on top of it.
+ *
+ * A key difference is that TypeScript is a statically typed language,
+ * while JavaScript is dynamically typed.
+ */
+
+console.log("Welcome to Typescript")
+
